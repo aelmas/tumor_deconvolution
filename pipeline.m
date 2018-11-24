ad8d05f6b5a1fe6c5586f130724c957d0fb7e77f
Abdulkadir Elmas <ae2321@columbia.edu>
2018-11-24 22:43:17 +0300

    Update pipeline.m

--- pipeline.m
+++ pipeline.m
@@ -1,4 +1,4 @@
-clear 
+clear; close all; clc
 %% 1. load data
 load data_MC3.mat
 load data_mRNA.mat
@@ -51,11 +51,12 @@ if flag_normalize_PRO
 end
 % check distributions
 figure(1);
-subplot(211); boxplot(values_mRNA); ylabel('mRNA');
-subplot(212); boxplot(values_PRO); ylabel('Proteome');
+subplot(211); boxplot(values_mRNA); ylabel('BRCA Transcriptome');
+subplot(212); boxplot(values_PRO); ylabel('BRCA Proteome');
 
 
 %% 3. deconvolution methods
+%
 %% 3.a. tumor transcriptome
 % xCell, performs cell type enrichment analysis from gene expression data for 64 
 %   immune and stroma cell types by reducing associations between closely related cell types.
@@ -89,8 +90,9 @@ data_mRNA_cleaned_xCell_result = readtable('xCell_result_BRCA_mRNA_formatted_nor
 % xCell has a bug that assigns the first variable to the row names | fix this
 data_mRNA_cleaned_xCell_result.Properties.VariableNames = data_mRNA_cleaned.Properties.VariableNames; 
 % display cell enrichments
-figure(2);
+figure(2); 
 heatmap(data_mRNA_cleaned_xCell_result.Properties.VariableNames,data_mRNA_cleaned_xCell_result.Properties.RowNames,table2array(data_mRNA_cleaned_xCell_result))
+title('BRCA Transcriptome cell enrichments')
 
 %% 3.b. tumor proteome
 % a quick observation shows that there are ~10.5K common genetic elements between the analyzed transcriptome and proteome data sets:
@@ -123,7 +125,66 @@ data_PRO_cleaned_xCell_result = readtable('xCell_result_BRCA_PRO_formatted_norma
 % xCell has a bug that assigns the first variable to the row names | fix this
 data_PRO_cleaned_xCell_result.Properties.VariableNames = data_PRO_cleaned.Properties.VariableNames; 
 % display cell enrichments
-figure(3);
+figure(3); 
 heatmap(data_PRO_cleaned_xCell_result.Properties.VariableNames,data_PRO_cleaned_xCell_result.Properties.RowNames,table2array(data_PRO_cleaned_xCell_result))
+title('BRCA Proteome cell enrichments')
 
-[common_genes_mRNA_PRO_cleaned, ig_mRNA_cleaned, ig_PRO_cleaned] = intersect(data_mRNA_cleaned.Properties.RowNames, data_PRO_cleaned.Properties.RowNames);
+%% 4. Expression patterns of mutation carriers and non-carriers
+% 4.a. analyze proteomes of mutation carriers vs non-carriers in key driver genes
+% for each key driver gene: define carriers and non-carriers, compare their proteomes
+%   then generate protein profile for that mutation showing significance bw carriers vs non-carriers
+protein_profiles = cell(size(data_MC3,1),1);
+for k = 1:size(data_MC3,1) %for each mutation gene
+    % define carriers and non-carriers of the mutation in mutation_gene(k) in MC3 data
+    carrier_true = table2array(data_MC3(k,:))~='wt';
+    if sum(carrier_true) > 1
+        % map those carriers/non-carriers in proteome data
+        [~, ip_carrier, ~] = intersect(data_PRO_cleaned.Properties.VariableNames, data_MC3.Properties.VariableNames(carrier_true));
+        [~, ip_noncarrier, ~] = intersect(data_PRO_cleaned.Properties.VariableNames, data_MC3.Properties.VariableNames(~carrier_true));
+        % test the null hypothesis (H) that the proteomes of the mutation carriers and non-carriers in mutation_gene(k) are not significantly different
+        % H = 1 means that the null hypothesis can be rejected at 5% significance level
+        H_proteins = ttest2(table2array(data_PRO_cleaned(:,ip_carrier))', table2array(data_PRO_cleaned(:,ip_noncarrier))')';
+        % any protein whose expression significantly differ (H=1) between mutation carriers and non-carriers
+        protein_profiles{k}.proteins =  data_PRO_cleaned.Properties.RowNames(H_proteins==1);
+        protein_profiles{k}.mutation_gene = data_MC3.Properties.RowNames(k);
+    end
+end
+% 4.b. analyze inferred cell types of mutation carriers vs non-carriers in key driver genes
+% for each key driver gene: define carriers and non-carriers, compare their
+% inferred cell enrichments then generate cell-type profile for that mutation showing significance bw carriers vs non-carriers
+cell_type_profiles = cell(size(data_MC3,1),1);
+for k = 1:size(data_MC3,1) %for each mutation gene
+    % define carriers and non-carriers of the mutation in mutation_gene(k) in MC3 data
+    carrier_true = table2array(data_MC3(k,:))~='wt';
+    if sum(carrier_true) > 1
+        % map those carriers/non-carriers in cell enrichments data
+        [~, ip_carrier, ~] = intersect(data_PRO_cleaned_xCell_result.Properties.VariableNames, data_MC3.Properties.VariableNames(carrier_true));
+        [~, ip_noncarrier, ~] = intersect(data_PRO_cleaned_xCell_result.Properties.VariableNames, data_MC3.Properties.VariableNames(~carrier_true));
+        % test the null hypothesis (H) that the inferred cell enrichments of the mutation carriers and non-carriers in mutation_gene(k) are not significantly different
+        % H = 1 means that the null hypothesis can be rejected at 5% significance level
+        H_cell_types = ttest2(table2array(data_PRO_cleaned_xCell_result(:,ip_carrier))', table2array(data_PRO_cleaned_xCell_result(:,ip_noncarrier))')';
+        % any cell-type whose enrichment scores significantly differ (H=1) between mutation carriers and non-carriers
+        cell_type_profiles{k}.cell_types =  data_PRO_cleaned_xCell_result.Properties.RowNames(H_cell_types==1);
+        cell_type_profiles{k}.mutation_gene = data_MC3.Properties.RowNames(k);
+    end
+end
+% ------------------------------------------------------------------------
+% Interpretation of analyses "4.a" and "4.b":
+% ------------------------------------------------------------------------
+k = 2;
+% For the mutation in the "k-th key driver gene":
+protein_profiles{k}.mutation_gene
+% the following list of "protein(s)" have different expression patterns between that mutation's carriers and non-carriers:
+protein_profiles{k}.proteins
+% and the following inferred "cell-type(s)" are enriched differently as well:
+cell_type_profiles{k}.cell_types
+%
+% That is, for k=2, the "ARID1A" gene's mutation-carriers and wild-types 
+% have different proteome signals on "374 proteins", which presumably
+% occured in the cell-type "Myocytes".
+%
+% For k=6, the "BRCA1" gene's mutation-carriers and wild-types 
+% have different proteome signals on "527 proteins", which presumably 
+% occured in the cell-types "CD4+ Tem", "CMP", "MPP", "pro B-cells", "Th2 cells".
+% ------------------------------------------------------------------------
+ 
